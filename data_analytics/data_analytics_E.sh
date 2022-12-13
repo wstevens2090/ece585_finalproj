23351ddd57f3357b6635886125ae70d617855aaa
wstevens2090 <ws17@princeton.edu>
2022-12-13 14:34:25 -0500

    New Data

--- data_analytics/data_analytics_E.sh
+++ data_analytics/data_analytics_E.sh
@@ -2,11 +2,11 @@
 
 
 # Step 2: Pin tasks to P cores. Run 1 master, 1 slave. 
-# docker run --rm -d --net hadoop-net --name master --hostname master cloudsuite3/data-analytics master
-# docker run --rm -d --net hadoop-net --name slave01 --hostname slave01 cloudsuite3/hadoop slave
+docker run --rm --cpuset-cpus 16-23 -d --net hadoop-net --name master --hostname master cloudsuite3/data-analytics master
+docker run --rm --cpuset-cpus 16-23 -d --net hadoop-net --name slave01 --hostname slave01 cloudsuite3/hadoop slave
 
 # Step 3: Run Execute; sleep for 60s. 
-taskset --cpu-list 16-23 docker exec master benchmark & 
+docker exec master benchmark & 
 sleep 60
 
 # Step 4: Record counters for 60s. Run with & to prevent blocking. 
