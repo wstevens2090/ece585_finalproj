be2272832fff994e78f441c80667c69133e8fe74
wstevens2090 <ws17@princeton.edu>
2022-12-13 15:17:09 -0500

    Pinned P Data

--- data_analytics/data_analytics_P.sh
+++ data_analytics/data_analytics_P.sh
@@ -2,13 +2,18 @@
 
 
 # Step 2: Pin tasks to P cores. Run 1 master, 1 slave. 
-docker run --rm --cpuset-cpus 0-15 -d --net hadoop-net --name master --hostname master cloudsuite3/data-analytics master
-docker run --rm --cpuset-cpus 0-15 -d --net hadoop-net --name slave01 --hostname slave01 cloudsuite3/hadoop slave
+docker run --rm --cpuset-cpus 0 -d --net hadoop-net --name master --hostname master cloudsuite3/data-analytics master
+
+for i in {1..15}
+do 
+    name=slave"$i"
+    docker run --rm --cpuset-cpus $i -d --net hadoop-net --name $name --hostname $name cloudsuite3/hadoop slave
+done    
 
 # Step 3: Run Execute; sleep for 60s. 
 docker exec master benchmark & 
 sleep 60
 
 # Step 4: Record counters for 60s. Run with & to prevent blocking. 
-bash ../perf_scripts/perf_counters_P.sh ./results/data_analytics_P.out 60
-bash ../perf_scripts/perf_counters_allP.sh ./results/data_analytics_P 60
+bash ../perf_scripts/perf_counters_P.sh ./results/data_analytics_P.out 60 & 
+bash ../perf_scripts/perf_counters_allP.sh ./results/data_analytics_P 60 &
